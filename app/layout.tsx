d47e8ab89bcbb21ad98113df6710ba50da8ce1ee
Refaldy Bagas Anggana <refaldybanggana@gmail.com>
2025-04-07 11:02:52 +0700

    create navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,21 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import NavBar from "@/components/layout/NavBar";
+import { ThemeProvider } from "next-themes";
 
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["400", "700"]
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
   title: "WebDevBlog",
   description: "Your favorite web dev blogs!",
   icons: {
-    icon: "/logo.svg"
-  }
+    icon: "/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -23,11 +25,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={cn("antialiased", poppins.variable)}
+        className={cn(
+          "antialiased flex flex-col min-h-screen px-2",
+          poppins.variable
+        )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          <main className="flex-grow">{children}</main>
+          <footer>...</footer>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/layout/Container.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="max-w-[1920px] w-full mx-auto px-4 py-4 xl:px-20">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
