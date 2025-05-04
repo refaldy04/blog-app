354ddbda15a69a0ffd6d33e3abbe5d2411d9ba59
Refaldy Bagas Anggana <refaldybanggana@gmail.com>
2025-05-04 08:01:23 +0700

    upload image to edgestore

--- components/blog/CreateBlogForm.tsx
+++ components/blog/CreateBlogForm.tsx
@@ -5,10 +5,15 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
 import FormField from "../common/FormField";
+import AddCover from "./AddCover";
+import { useState } from "react";
 
 const CreateBlogForm = () => {
   const session = useSession();
   const userId = session.data?.user.userId;
+  const [uploadedCover, setUploadedCover] = useState<string>();
+
+  console.log(uploadedCover);
 
   const {
     register,
@@ -26,6 +31,7 @@ const CreateBlogForm = () => {
   return (
     <form className="flex flex-col justify-between max-w-[1200px] m-auto min-h-[85vh]">
       <div>
+        <AddCover setUploadedCover={setUploadedCover} />
         <FormField
           id="title"
           register={register}
